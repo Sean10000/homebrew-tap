60435e3e9c314eb54c95f50c4b1f54424b9a25e8
Sean10000 <mashicheng001@163.com>
2026-04-23 14:22:03 +0800

    chore: bump LaunchManager to v1.1.0

--- Casks/launchmanager.rb
+++ Casks/launchmanager.rb
@@ -1,8 +1,8 @@
 cask "launchmanager" do
-  version "1.0.0"
-  sha256 "fd147ecd13f7b08d11d46db41c247bcfe73cff8352c79ce9abf78fa49d800afe"
+  version "1.1.0"
+  sha256 "e9b4faf4474afc67204a520fa398dde176782831b592aaa6f113f1804c704a18"
 
-  url "https://github.com/Sean10000/LaunchManager/releases/download/v#{version}/LaunchManager.dmg"
+  url "https://github.com/Sean10000/LaunchManager/releases/download/v\#{version}/LaunchManager.dmg"
   name "LaunchManager"
   desc "macOS app for managing launchd LaunchAgents and LaunchDaemons"
   homepage "https://github.com/Sean10000/LaunchManager"
