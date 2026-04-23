1a09fb69562cab5c87430e44061abce3dfc86657
Sean10000 <mashicheng001@163.com>
2026-04-24 00:22:36 +0800

    fix: remove escaped backslash from url interpolation

--- Casks/launchmanager.rb
+++ Casks/launchmanager.rb
@@ -2,7 +2,7 @@ cask "launchmanager" do
   version "1.2.0"
   sha256 "739795b0f43d577c1ca0961cc676b19837deb9db12a45e717bd890acb0e64c0a"
 
-  url "https://github.com/Sean10000/LaunchManager/releases/download/v\#{version}/LaunchManager.dmg"
+  url "https://github.com/Sean10000/LaunchManager/releases/download/v#{version}/LaunchManager.dmg"
   name "LaunchManager"
   desc "macOS app for managing launchd LaunchAgents and LaunchDaemons"
   homepage "https://github.com/Sean10000/LaunchManager"
