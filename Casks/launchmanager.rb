a1e3b8f52b4af1355dcbf0aaaba6102ce7fceb2b
Sean10000 <mashicheng001@163.com>
2026-04-23 17:27:31 +0800

    chore: bump LaunchManager to v1.1.2

--- Casks/launchmanager.rb
+++ Casks/launchmanager.rb
@@ -1,6 +1,6 @@
 cask "launchmanager" do
-  version "1.1.1"
-  sha256 "537bc923acd45fa5f5c4f10b3b270b8ed2bf0f41747859df3d2aa67e21f234ac"
+  version "1.1.2"
+  sha256 "ebe8d3da7ae5b488cf4e8fab4411ba66c7b18a6956b35145400199f01a2263cc"
 
   url "https://github.com/Sean10000/LaunchManager/releases/download/v\#{version}/LaunchManager.dmg"
   name "LaunchManager"
